20c9cca4eae785d47ba379dac794dedf0f75cc99
Alexander Snorkin <Alexander.Snorkin@acronis.com>
2020-12-28 21:49:29 +0300

    submit.py cache_images=False

--- submit.py
+++ submit.py
@@ -34,7 +34,7 @@ def create_batch_generator(args, model_config):
         ToTensorV2(),
     ])
 
-    dataset = InferenceXRayDataset.create(args.images_dir, cache_images=True, transform=transform)
+    dataset = InferenceXRayDataset.create(args.images_dir, cache_images=False, transform=transform)
 
     return DataLoader(
         dataset,
