26addbe3ad7525a1a351887715ce1feb4f4c2e7a
Alexander Snorkin <Alexander.Snorkin@acronis.com>
2020-12-29 21:22:07 +0300

    submit.py normalization added

--- submit.py
+++ submit.py
@@ -94,6 +94,7 @@ def main(args):
     # Aggregate
     predictions = torch.cat(predictions, dim=1)
     predictions = rank_average(*predictions)
+    predictions = predictions / predictions.max()
     predictions = predictions.cpu().numpy()
 
     # Save submission
