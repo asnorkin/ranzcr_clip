submit.py rank_average fixes: float32, normalization

classification/loss.py
@@ -42,9 +42,11 @@ def batch_auc_roc(targets, probabilities):
 def rank_average(*tensors):
     assert len(tensors) > 1
 
+    norm = len(tensors) * len(tensors[0])
+
     result = torch.zeros_like(tensors[0])
     for tensor in tensors:
-        result += torch.argsort(tensor, dim=0) / len(tensors)
+        result += torch.argsort(tensor, dim=0) / norm
 
     return result
 

submit.py
@@ -92,9 +92,8 @@ def main(args):
         image_uids.extend(batch['instance_uid'])
 
     # Aggregate
-    predictions = torch.cat(predictions, dim=1)
+    predictions = torch.cat(predictions, dim=1).to(torch.float32)
     predictions = rank_average(*predictions)
-    predictions = predictions / predictions.max()
     predictions = predictions.cpu().numpy()
 
     # Save submission