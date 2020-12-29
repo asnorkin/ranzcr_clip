2a383ac294f0ee87d7151e4c9b53a1171519ae00
Alexander Snorkin <Alexander.Snorkin@acronis.com>
2020-12-29 19:02:14 +0300

    run_train_single_model_classification.sh EPOCH=30, NUM_WORKERS=16, SMOOTHING=0.05

--- run_train_single_model_classification.sh
+++ run_train_single_model_classification.sh
@@ -1,14 +1,15 @@
 WORK_DIR=classification
 LR=1e-3
-EPOCHS=20
+EPOCHS=30
 BATCH_SIZE=48
-NUM_WORKERS=32
+NUM_WORKERS=16
 PRECISION=16
 VAL_SIZE=0.2
+SMOOTHING=0.05
 
 PROJECT=efficientnet_b0
 
-EXPERIMENT=${PROJECT}_single_model_${LR}lr${VAL_SIZE}val${EPOCHS}e${BATCH_SIZE}b${PRECISION}p
+EXPERIMENT=${PROJECT}_single_model_${LR}lr${SMOOTHING}smt${VAL_SIZE}val${EPOCHS}e${BATCH_SIZE}b${PRECISION}p
 GPUS=0,1
 
 # Add repo root to PYTHONPATH for imports
@@ -24,6 +25,7 @@ nohup python classification/train.py \
     --gpus=${GPUS} \
     --num_epochs=${EPOCHS} \
     --val_size=${VAL_SIZE} \
+    --smoothing_epsilon=${SMOOTHING} \
     --accelerator=ddp \
     --precision=${PRECISION} \
     --cache_images \
