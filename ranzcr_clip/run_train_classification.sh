d3ae5a70c9d16227e68677a48c5c0392b9f63e93
Alexander Snorkin <Alexander.Snorkin@acronis.com>
2021-01-14 18:36:57 +0300

    run_train_classification.sh update

--- ranzcr_clip/run_train_classification.sh
+++ ranzcr_clip/run_train_classification.sh
@@ -1,13 +1,14 @@
 WORK_DIR=classification
-LR=1e-3
-EPOCHS=30
+LR=1e-4
+EPOCHS=50
 BATCH_SIZE=48
+ACC_BATCHES=1
 NUM_WORKERS=15
 PRECISION=16
 
 PROJECT=efficientnet_b0
 
-EXPERIMENT=${PROJECT}_${LR}lr${EPOCHS}e${BATCH_SIZE}b${PRECISION}p
+EXPERIMENT=${PROJECT}_${LR}lr${EPOCHS}e${BATCH_SIZE}x${ACC_BATCHES}b${PRECISION}p
 GPUS=0,1
 
 # Train outputs dir
@@ -24,11 +25,11 @@ nohup python classification/train.py \
     --work_dir=${WORK_DIR} \
     --experiment=${EXPERIMENT} \
     --batch_size=${BATCH_SIZE} \
+    --accumulate_grad_batches=${ACC_BATCHES} \
     --num_workers=${NUM_WORKERS} \
     --lr=${LR} \
     --gpus=${GPUS} \
     --num_epochs=${EPOCHS} \
-    --val_type=single \
     --accelerator=ddp \
     --precision=${PRECISION} \
     --use_tta \
