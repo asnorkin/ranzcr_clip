7f7f4c08aabb6c489725d878914fead96a2f3f78
Alexander Snorkin <Alexander.Snorkin@acronis.com>
2021-01-18 22:47:45 +0300

    train_segmentation.sh added; run_train_classification.sh -> train_classification.sh

---
+++ ranzcr_clip/train_segmentation.sh
@@ -0,0 +1,35 @@
+WORK_DIR=segmentation
+LR=1e-3
+EPOCHS=50
+BATCH_SIZE=16
+ACC_BATCHES=2
+NUM_WORKERS=15
+PRECISION=16
+
+PROJECT=efficientnet_b0
+
+EXPERIMENT=${PROJECT}_${LR}lr${EPOCHS}e${BATCH_SIZE}x${ACC_BATCHES}b${PRECISION}p
+GPUS=0,1
+
+# Train outputs dir
+TRAIN_OUTPUTS_DIR=${WORK_DIR}/train_outputs
+if [ ! -d ${TRAIN_OUTPUTS_DIR} ]; then
+    mkdir ${TRAIN_OUTPUTS_DIR}
+fi
+
+# Add repo root to PYTHONPATH for imports
+export PYTHONPATH=.:${PYTHONPATH}
+
+nohup python classification/train.py \
+    --project=${PROJECT} \
+    --work_dir=${WORK_DIR} \
+    --experiment=${EXPERIMENT} \
+    --batch_size=${BATCH_SIZE} \
+    --accumulate_grad_batches=${ACC_BATCHES} \
+    --num_workers=${NUM_WORKERS} \
+    --lr=${LR} \
+    --gpus=${GPUS} \
+    --num_epochs=${EPOCHS} \
+    --accelerator=ddp \
+    --precision=${PRECISION} \
+    &> ${TRAIN_OUTPUTS_DIR}/${EXPERIMENT}.log 2>&1 &
