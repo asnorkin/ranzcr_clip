505eae786404349e9d15abbcf5059bac9b632151
Alexander Snorkin <Alexander.Snorkin@acronis.com>
2021-03-08 20:47:32 +0300

    Predictor updated: segmentation support; predict() method; CLAHE transform added

--- ranzcr_clip/predictors/predictor.py
+++ ranzcr_clip/predictors/predictor.py
@@ -1,18 +1,31 @@
 import os
 import os.path as osp
+from typing import Dict, List, Union
 
 import albumentations as A
+import albumentations.augmentations.functional as AF
 import numpy as np
 import torch
 from albumentations.pytorch.transforms import ToTensorV2
+from pytorch_lightning import LightningModule
 from torch.backends import cudnn
 
-from classification.modelzoo import ModelConfig
 from classification.module import XRayClassificationModule
+from common.model_utils import ModelConfig
 
 
 class Predictor:
-    def predict_batch(self, batch: dict) -> torch.Tensor:
+    def predict(self, sample: Union[Dict, np.ndarray], *args, **kwargs):
+        if isinstance(sample, dict):
+            batch = {key: [val] for key, val in sample.items()}
+        elif isinstance(sample, np.ndarray):
+            batch = {'image': [sample]}
+        else:
+            raise TypeError(f'Unexpected type of sample: {type(sample)}')
+
+        return self.predict_batch(batch, *args, **kwargs)[0]
+
+    def predict_batch(self, batch: Union[Dict, List], *args, **kwargs) -> torch.Tensor:
         raise NotImplementedError
 
 
@@ -69,16 +82,23 @@ class TorchModelPredictor(TorchModelMixin, Predictor):
 
         self.transform = A.Compose(
             [
-                A.Resize(height=self.config.input_height, width=self.config.input_width),
-                A.Normalize(max_pixel_value=1.0),
-                ToTensorV2(),
+                A.Resize(height=self.config.input_height, width=self.config.input_width, always_apply=True),
+                A.CLAHE(always_apply=True),
+                A.Normalize(mean=0.482, std=0.220, always_apply=True),  # Ranzcr
+                ToTensorV2(always_apply=True),
             ]
         )
 
     def predict_batch(
-        self, batch, preprocess: bool = False, output: str = 'probabilities', tta: bool = True
+        self, batch: Union[Dict, List], preprocess: bool = False, output: str = 'probabilities', tta: bool = True
     ) -> torch.Tensor:
         assert output in {'logits', 'probabilities', 'binary'}
+
+        if isinstance(batch, list):
+            batch = {'image': batch}
+
+        # Save original shapes
+        batch['ori_shape'] = [image.shape for image in batch['image']]
 
         # Preprocess
         if preprocess:
@@ -102,16 +122,35 @@ class TorchModelPredictor(TorchModelMixin, Predictor):
             return logits
 
         # Postprocess
-        predictions = torch.sigmoid(logits)
+        predictions = torch.sigmoid(logits).cpu().numpy()
 
         if output == 'binary':
             predictions[predictions < self.config.confidence_threshold] = 0
             predictions[predictions >= self.config.confidence_threshold] = 1
+            predictions = predictions.astype(int)
+
+        # Segmentation masks postprocess
+        if len(predictions.shape) > 2:
+            # Prepare masks
+            predictions = (predictions * 255).astype(np.uint8).transpose(0, 2, 3, 1)
+            if predictions.shape[-1] == 1:
+                predictions = predictions[..., 0]
+
+            # Resize to the original size
+            resized_predictions = []
+            for i, prediction in enumerate(predictions):
+                ori_height, ori_width = batch['ori_shape'][i]
+                resized_predictions.append(AF.resize(prediction, height=ori_height, width=ori_width))
+            predictions = np.stack(resized_predictions)
+
+            # Fix resize error
+            predictions[predictions < 128] = 0
+            predictions[predictions >= 128] = 255
 
         return predictions
 
     @classmethod
-    def create_from_checkpoints(cls, checkpoints_dir: str) -> Predictor:
+    def create_from_checkpoints(cls, checkpoints_dir: str, module_class: LightningModule = XRayClassificationModule):
         config = ModelConfig(osp.join(checkpoints_dir, 'config.yml'))
         model_files = [
             osp.join(checkpoints_dir, fname) for fname in os.listdir(checkpoints_dir) if fname.endswith('.ckpt')
@@ -123,5 +162,5 @@ class TorchModelPredictor(TorchModelMixin, Predictor):
         if len(model_files) != 1:
             raise RuntimeError(f'Found non unique single model checkpoint: {model_files}')
 
-        model = XRayClassificationModule.build_model(config, model_files[0])
+        model = module_class.build_model(config, model_files[0])
         return cls(model, config)
